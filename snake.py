d66fe406dc73fc88e2be0f161e82fd7094ebb943
Mauplz <A01783203@tec.mx>
2022-05-12 12:58:07 -0500

    Snake con cambio de colores

--- snake.py
+++ snake.py
@@ -8,14 +8,17 @@ Exercises
 4. Change the snake to respond to arrow keys.
 
 """
-
+import random
 from turtle import *
 from random import randrange
 from freegames import square, vector
 
+
+
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+
 
 def change(x, y):
     "Change snake direction."
@@ -30,6 +33,7 @@ def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
+    colors = ['blue','green','yellow','brown','grey']
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -50,7 +54,10 @@ def move():
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
-    square(food.x, food.y, 9, 'green')
+    for body in snake:
+        square(head.x, head.y, 9, random.choice(colors))
+
+    square(food.x, food.y, 9, random.choice(colors))
     update()
     ontimer(move, 100)
 
